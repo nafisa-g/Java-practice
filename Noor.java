5c3c1427d10375e9c0a4c3bd47d789da78992b34
nafisa-g <nafisagagguturi@gmail.com>
2024-01-18 07:47:00 +0530

    Assignment Operations

--- Noor.java
+++ Noor.java
@@ -1,6 +1,40 @@
 public class Noor {
     public static void main(String[] args) {
-        System.out.println("BISMIALLAH");
+        
+      /*   int num1=5;
+        int num2=10;
+        int result=num1+num2;
+        System.out.println(result);
+
+        int result=num1-num2;
+        System.out.println(result);
+
+        int result=num1*num2;
+        System.out.println(result);
+
+        int result=num1/num2;
+        System.out.println(result);
+
+        int result=num1%num2;
+        System.out.println(result);  */
+
+
+
+
+        int num=1;
+        num++;
+        System.out.println(num);
+
+        ++num;
+        System.out.println(num);   
+
+
+
+        int result=num++;
+        System.out.println(num);
+
+      
+    }
     }
     
-}
+
