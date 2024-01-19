468ade119ef37cf66758cb3a27af34c3718316d4
nafisa-g <nafisagagguturi@gmail.com>
2024-01-19 20:13:16 +0530

    Assignment Operation

--- Yahid.java
+++ Yahid.java
@@ -1,6 +1,69 @@
 public class Yahid {
-    public system void main(String[]args){
+    public static void main(String[]args){
+
         
+        //Assidnment Operation 
+         int num1=1;
+        int num2=2;
+
+        int result  =num1+num2;
+        System.out.println(result );
+
+        int check=num1-num2;
+        System.out.println(check);
+
+        int  turn =num1*num2 ;
+        System.out.println( turn );
+
+        int  out =num1/num2;
+        System.out.println(out ); 
+
+        int  come =num1*num2 ;
+        System.out.println( come);
+
+
+
+        int num=5;
+
+        num=num1+1;
+        System.out.println(num);
+
+        num=num1-1;
+        System.out.println(num);
+
+        num=num1*1;
+        System.out.println(num);
+
+        num=num1/1;
+        System.out.println(num);
+
+        num=num1%1;
+        System.out.println(num);  
+
+
+
+        //post increment
+        int A=1;
+        A++;
+        System.out.println(A); 
+
+        //Increment and fetch the values
+        int nuM=21;
+        int Demo=++nuM;
+        System.out.println(Demo);
+
+        //per increment
+        int B=2;
+        B++;
+        System.out.println(B);
+
+
+         //Increment and fetch the values
+         int Num=9;
+         int pin=Num++;
+         System.out.println(pin);
+
     }
+
     
 }
