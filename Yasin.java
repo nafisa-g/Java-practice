0e1bd6e6803df3ac6d9b3c6bc6f0b01f4b3b14fe
nafisa-g <nafisagagguturi@gmail.com>
2024-01-19 19:36:21 +0530

    Date types

---
+++ Yasin.java
@@ -0,0 +1,20 @@
+public class Yasin {
+    public static void main(String[]args){
+        int num=4;
+        long l=8;
+        short sh=12;
+        byte by=16;
+
+        float f=5f;
+        double d=10;
+
+        char c='a';
+
+        boolean b=true;
+
+
+        System.out.println(num);
+
+    }
+    
+}
