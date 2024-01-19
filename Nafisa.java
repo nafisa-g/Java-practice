ad091b0e30c6716e4b057661aa197a75d702045e
nafisa-g <nafisagagguturi@gmail.com>
2024-01-19 21:06:24 +0530

    Relational Operation

---
+++ Nafisa.java
@@ -0,0 +1,43 @@
+public class Nafisa {
+    public static void main(String[]args)
+    {
+
+        //Relational Operation
+      int x=2;
+      int y=4;
+
+       boolean result=x<y;
+      System.out.println(result);
+
+      boolean pin=x>y;
+      System.out.println(pin);
+
+      boolean out=x<=y;
+      System.out.println(out);
+
+      boolean come=x>=y;
+      System.out.println(come);
+
+      boolean p=x!=y;
+      System.out.println(p);
+
+      boolean q=x==y;
+      System.out.println(q); 
+
+
+
+      float r=12;
+      double s=24;
+  
+      boolean t=r<s;
+      System.out.println(t);
+      
+      boolean v=r>s;
+      System.out.println(v);
+      
+      
+      
+
+    }
+    
+}
