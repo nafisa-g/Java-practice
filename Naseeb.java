26e9be49d12403a71abebff0b62b4f2edee4c523
nafisa-g <nafisagagguturi@gmail.com>
2024-01-20 08:56:56 +0530

    if -else

---
+++ Naseeb.java
@@ -0,0 +1,14 @@
+public class Naseeb {
+    public static void main(String[]args){
+        int x=1; 
+       if(x>45)
+       {
+        System.out.println("Bismiallah");
+       }
+       else
+       {
+        System.out.println("Allah");
+       }
+    }
+    
+}
