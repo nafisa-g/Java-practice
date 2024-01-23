5c0d3e9ffbff3224f4437c61ba6cece24d47b12f
nafisa-g <nafisagagguturi@gmail.com>
2024-01-23 10:55:03 +0530

    Ternary Operator

---
+++ Upload.java
@@ -0,0 +1,14 @@
+public class Upload {
+    public static void main(String[]args){
+        //Ternary Operator
+        //You can use If else or Ternary Operator
+
+        //even or odd number
+        int n=4;
+        int result=2;
+        result=(n%2==0) ? 10 : 11 ;
+        System.out.println(result);
+
+    }
+    
+}
