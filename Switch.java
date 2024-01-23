dc1db3d5f345ba428c3a7944d40140eff85beccf
nafisa-g <nafisagagguturi@gmail.com>
2024-01-23 11:07:57 +0530

    Switch Statement

---
+++ Switch.java
@@ -0,0 +1,35 @@
+public class Switch {
+    public static void main(String[] args) {
+
+        //Switch
+        //old Version of Switch in java
+
+        int n=4;
+        switch(n){
+            case 0:
+            System.out.println("Monday");
+            break;
+            case 1:
+            System.out.println("Tuesday");
+            break;
+            case 2:
+            System.out.println("Wednesday");
+            break;
+            case 3:
+            System.out.println("Thursday");
+            break;
+            case 4:
+            System.out.println("Friday");
+            break;
+            case 5:
+            System.out.println("Saturday");
+            break;
+            case 6:
+            System.out.println("Sunday");
+            break;
+            default:
+            System.out.println("In valid number");
+        }
+    }
+    
+}
