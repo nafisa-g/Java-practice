9c8208dcd371b95e9acf0f58c9b447cd6ccc629d
nafisa-g <nafisagagguturi@gmail.com>
2024-01-23 10:45:53 +0530

    If Else If

---
+++ Updata.java
@@ -0,0 +1,25 @@
+public class Updata {
+    public static void main(String[]args){
+        // If else If
+        int x=1;
+        int y=2;
+        int z=3;
+            if(x<y && x>z)
+
+            System.out.println("Noor Mohammad");
+    
+            else if (y<z)
+        
+            System.out.println("Yasin Basha") ;
+        
+            else
+        
+            System.out.println("Yahid bro");
+
+             
+
+        }
+        
+    }
+    
+
