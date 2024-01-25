0f1f4c20b3a3a87683c86359eed326933cc9a80f
nafisa-g <nafisagagguturi@gmail.com>
2024-01-25 08:09:11 +0530

    New switch

---
+++ Samu.java
@@ -0,0 +1,19 @@
+public class samu {
+    public static void main(String[]args) { 
+
+        
+    //New switch code in Java
+
+    String day="Sunday";
+    switch(day)
+    {
+        case"Saturday","sunday"->System.out.println("6am");
+        case"monday"->System.out.println("7am");
+        case"Tuesday"->System.out.println("8am");
+default ->System.out.println("5am");
+
+    }
+}
+
+    
+}
