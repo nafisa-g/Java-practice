b0bed988bc9ff57c9b5dea9acd61390fca66c870
nafisa-g <nafisagagguturi@gmail.com>
2024-01-28 16:06:06 +0530

    For loop

---
+++ Basha.java
@@ -0,0 +1,10 @@
+public class Basha {
+    public static void main(String[] args) {
+        //for loop
+      //  int i=5;
+        for(int i=0;i<3;i++){
+System.out.println("Noor Mohammad"+i);
+        }
+    }
+    
+}
