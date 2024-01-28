510e6c8f084e3ed8ff8917e9c4877328c1d84f86
nafisa-g <nafisagagguturi@gmail.com>
2024-01-28 15:59:02 +0530

    Do loop

---
+++ Dot.java
@@ -0,0 +1,12 @@
+public class Dot {
+    public static void main(String[]args) {
+        //Do loop
+        int i=1;
+        do
+        {
+            System.out.println("Hi"+i);
+            i++;
+        }while(i<=10);
+    }
+    
+}
