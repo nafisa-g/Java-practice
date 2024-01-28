4dfc5c6ad8097903277255a8e788ee202d1ffe3b
nafisa-g <nafisagagguturi@gmail.com>
2024-01-28 15:51:17 +0530

    While loop

---
+++ Computer.java
@@ -0,0 +1,14 @@
+public class Computer {
+    public static void main(String[]args){
+        //while loop
+        int i=1;
+        while(i<=4)
+        {
+            System.out.println("hi"+i);
+            i++;
+        }
+        System.out.println("Bye"+i);
+    }
+  
+    
+}
