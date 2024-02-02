92f9983dd91dfb5d8a25ab2590afab3aa064cf38
nafisa-g <nafisagagguturi@gmail.com>
2024-02-02 12:12:20 +0530

    class and  object

---
+++ qwert.java
@@ -0,0 +1,26 @@
+//public class qwert {
+  //  public static void main(String[]args){
+
+    class computer{
+      public int add(int n1,int n2)
+      {
+        int r=n1-n2;
+        return r;
+
+      }
+    }
+
+public class qwert{
+  public static void main(String[]args){
+    computer con =new computer();
+    int result=con.add(7,5);
+    System.out.println(result);
+  }
+}
+
+
+
+    
+
+    
+
