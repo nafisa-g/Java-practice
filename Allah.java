86985e7b1f0938811e9586a199f4e5a4c9561d8b
nafisa-g <nafisagagguturi@gmail.com>
2024-02-13 12:03:59 +0530

    Encapsulation

---
+++ Allah.java
@@ -0,0 +1,27 @@
+
+
+class Human{
+       private int Age=20;
+       private  String Name="Mohammad";
+       
+        public int getAge()
+       {
+        return Age;
+       }
+       public String getName()
+       {
+        return Name;
+       }
+    }
+       public class Allah{
+        public static void main(String[] args) {
+
+            Human obj=new Human();
+            System.out.println(obj.getAge()+" : "+obj.getName());
+       }
+    }
+
+
+
+
+  
