dd43428b02b0912055b78b4cc9717459abf7d40a
nafisa-g <nafisagagguturi@gmail.com>
2024-02-27 06:04:43 +0530

    String....

---
+++ Yaseen/Zxccv.java
@@ -0,0 +1,32 @@
+public class Zxccv {
+    public static void main(String[] args) {
+
+
+
+        //String
+       String name="Bismiallah";
+        System.out.println(name);
+
+        String nam="Bismiallah";
+        System.out.println(name.charAt(0));
+
+        String book=new String("navin");
+        System.out.println(book.concat("reddy"));
+
+
+
+
+        //StringBuffer
+        StringBuffer sb=new StringBuffer();
+        System.out.println(sb.capacity()); 
+
+        StringBuffer str=new StringBuffer("Allah");
+        System.out.println(str.capacity()); 
+         
+         
+
+        StringBuffer sa=new  StringBuffer("Sameera");
+        sb.append("Reddy");
+        System.out.println(sa);
+    }
+}
