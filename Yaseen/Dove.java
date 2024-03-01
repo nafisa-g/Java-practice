6b1eb6f3f4b0fe9cf2854897a8571939967bb669
nafisa-g <nafisagagguturi@gmail.com>
2024-03-01 19:26:52 +0530

    Multi level Inheritance

---
+++ Yaseen/Dove.java
@@ -0,0 +1,4 @@
+class Mobile {
+}
+
+public class
--- Yaseen/Oxford.java
+++ Yaseen/Oxford.java
@@ -1,28 +1,21 @@
 import  java .util.Scanner;
-public class Book {
+public class Oxford {
     public static void main(String[] args) {
         Scanner q=new Scanner(System.in);
      System.out.println("Enter two numbers");
     String str=q.nextLine();
-       String rev="  ";
+       String rev="Naseen";
 
     for(int i=0;i<str.length();i++)
     rev=str.charAt(i)+rev;
    System.out.println(rev);
 
 
-      if (rev.equals(str));
+      if (rev.equals(str))
     System.out.println("palindrome");
    else{
   
     System.out.println(" NOt  palindrome");
     }
     
-   
-
-
-
-}
-    }
-    
-
+    }}
