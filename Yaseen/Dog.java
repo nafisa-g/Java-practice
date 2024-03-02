b329c39fb5e04ac930b9100a562b121d612a4165
nafisa-g <nafisagagguturi@gmail.com>
2024-03-02 08:36:31 +0530

    Inheritance

--- Yaseen/Dog.java
+++ Yaseen/Dog.java
@@ -1,11 +1,34 @@
+class A{
+  public int add(int i,int j){
+    return i+j;
+  }
+ public int sub(int i,int j){
+    return i-j;
+
+  }
+}
+  class B extends A{
+    public int multi(int i,int j)
+    {
+      return i*j;
+    }
+
+       public int div(int i,int j)
+    {
+      return i/j;
+    }
+  }
+
+
+
 public class Dog extends C {
   public static void main(String[] args) {
     C obj=new C();
     int r1=obj.add(1,2);
     int r2=obj.sub(1,2);
-    int r3=obj.mui(1,2);
+    int r3=obj.multi(1,2);
     int r4=obj.div(1,2);
-    double r5=obj.power(1,2);
-    System.out.println(r1+":"+r2+":"+r3+":"+r4+":"+r5);
+    
+    System.out.println(r1+":"+r2+":"+r3+":"+r4);
   }
 }
