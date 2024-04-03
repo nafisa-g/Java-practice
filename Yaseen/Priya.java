7e94c9d5c03be0b9660cc766b3d66dee456aab86
nafisa-g <nafisagagguturi@gmail.com>
2024-04-03 16:43:18 +0530

    List

---
+++ Yaseen/Priya.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Vector;
+import java.util.List;
+
+
+public class Priya {
+    public static void main(String[] args) {
+
+        ArrayList<Integer>a=new ArrayList<Integer>();
+        LinkedList<String>b =new LinkedList<String>();
+        Vector<Float>c =new Vector<Float>();
+
+            a.add(12);
+            a.add(24);
+            b.add("Naseeb");
+            b.add("jan");
+            c.add(1.54f);
+            c.add(45.2f);
+
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+
+
+
+
+
+
+    }
+}
